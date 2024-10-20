33f9e55ff2619b454532d8d6ebe1799c5fda29cd
loinq2704 <nguyenquangloi2704@gmail.com>
2024-10-20 21:23:11 +0700

    Add bottom navigation

--- app/src/main/java/com/loinq/unnn/MainActivity.java
+++ app/src/main/java/com/loinq/unnn/MainActivity.java
@@ -1,24 +1,64 @@
 package com.loinq.unnn;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    private void bindingView() {
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+    }
+
+    private void bindingAcion(){
+        bottomNavigationView.setOnItemSelectedListener(navListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        bindingView();
     }
+
+    private BottomNavigationView.OnItemSelectedListener navListener =
+            new NavigationBarView.OnItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    if(item.getItemId() == R.id.nav_home)
+                        selectedFragment = new HomeFragment();
+                    else if(item.getItemId() == R.id.nav_infor)
+                        selectedFragment = new HomeFragment();
+                    else if(item.getItemId() == R.id.nav_settings)
+                        selectedFragment = new HomeFragment();
+
+                    // Load the selected fragment
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragmentContainerView, selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
